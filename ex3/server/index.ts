255a2bda43b7f07f347fe069c584fc1786ecfd1e
pdTrade <pd.pub.dev@gmail.com>
2024-06-10 16:39:37 +0900

    DDBへの登録処理

--- ex3/server/index.ts
+++ ex3/server/index.ts
@@ -3,9 +3,13 @@ import cors from "cors";
 import {
   DynamoDBDocumentClient,
   QueryCommandInput,
-  QueryCommand
+  QueryCommand,
+  PutCommandInput,
+  PutCommand,
 } from "@aws-sdk/lib-dynamodb";
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+
+import { v4 as uuid } from "uuid";
 
 const app = express();
 
@@ -18,6 +22,7 @@ const docClient = DynamoDBDocumentClient.from(ddbClient);
 app.get("/healthcheck", (_, res) => res.status(200).send(JSON.stringify("OK")));
 
 app.get("/", async (_, res) => {
+  console.log("get");
   const params: QueryCommandInput = {
     TableName: "main_table",
     ExpressionAttributeNames: {
@@ -34,6 +39,30 @@ app.get("/", async (_, res) => {
   const { Items } = await docClient.send(cmd);
 
   res.status(200).send({ todos: Items });
+});
+
+app.post("/", async (req, res) => {
+  const { name, description } = req.body.todo;
+
+  const todo = {
+    sort_key: uuid(),
+    partition_key: "todo",
+    name,
+    description,
+  };
+
+  const params: PutCommandInput = {
+    TableName: "main_table",
+    Item: todo,
+  };
+
+  const cmd = new PutCommand(params);
+
+  const ret = await docClient.send(cmd);
+
+  res.status(200).send({
+    todo,
+  });
 });
 
 app.listen(80, () => {
