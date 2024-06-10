b986b28d2bf21da9c3eed9532d9145f001f9bb36
pdTrade <pd.pub.dev@gmail.com>
2024-06-10 21:20:22 +0900

    S3静的ホスティング

--- ex3/infrastructure/lib/constructs/S3.ts
+++ ex3/infrastructure/lib/constructs/S3.ts
@@ -1,20 +1,39 @@
-import { RemovalPolicy } from "aws-cdk-lib";
-import { Bucket} from "aws-cdk-lib/aws-s3";
+import { CfnOutput, RemovalPolicy } from "aws-cdk-lib";
+import {
+  BlockPublicAccess,
+  Bucket,
+  BucketAccessControl,
+} from "aws-cdk-lib/aws-s3";
+import { BucketDeployment, Source } from "aws-cdk-lib/aws-s3-deployment";
 import { Construct } from "constructs";
+import { resolve } from "path";
 
 export class S3 extends Construct {
-  public readonly web_bucket: Bucket
-  
-  constructor(scope: Construct, id: string) {
-    super(scope, id)
+  public readonly web_bucket: Bucket;
 
-    this.web_bucket = new Bucket(scope, 'WebBucket', {
+  public readonly web_bucket_deployment;
+
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+
+    this.web_bucket = new Bucket(scope, "WebBucket", {
+      publicReadAccess: true,
+      blockPublicAccess: BlockPublicAccess.BLOCK_ACLS,
+      websiteIndexDocument: "index.html",
+      // websiteErrorDocument: "error.html",
       removalPolicy: RemovalPolicy.DESTROY,
       autoDeleteObjects: true,
-
     });
 
+    this.web_bucket_deployment = new BucketDeployment(
+      scope,
+      "WebBucketDeployment",
+      {
+        sources: [Source.asset(resolve(__dirname, "../../..", "web"))],
+        destinationBucket: this.web_bucket,
+      }
+    );
 
-
+    new CfnOutput(scope, "WebURL", { value: this.web_bucket.bucketWebsiteUrl });
   }
 }
--- ex3/infrastructure/lib/infrastructure-stack.ts
+++ ex3/infrastructure/lib/infrastructure-stack.ts
@@ -2,11 +2,14 @@ import { Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { Dynamodb } from "./constructs/Dynamodb";
 import { ECS } from "./constructs/ECS";
+import { S3 } from "./constructs/S3";
 
 export class InfrastructureStack extends Stack {
   public readonly dynamodb: Dynamodb;
 
   public readonly ecs: ECS;
+
+  public readonly s3: S3;
 
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
@@ -16,5 +19,7 @@ export class InfrastructureStack extends Stack {
     this.ecs = new ECS(this, "ECS", {
       dynamodb: this.dynamodb,
     });
+
+    this.s3 = new S3(this, 'S3')
   }
 }
