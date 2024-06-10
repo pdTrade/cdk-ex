9797d39ce3033d1a6048f41318a2ef91874a0483
pdTrade <pd.pub.dev@gmail.com>
2024-06-10 13:12:13 +0900

    ECS追加

--- ex3/infrastructure/lib/infrastructure-stack.ts
+++ ex3/infrastructure/lib/infrastructure-stack.ts
@@ -1,14 +1,20 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import { Dynamodb } from './constructs/Dynamodb';
+import { Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { Dynamodb } from "./constructs/Dynamodb";
+import { ECS } from "./constructs/ECS";
 
 export class InfrastructureStack extends Stack {
-  public readonly dynamodb: Dynamodb
+  public readonly dynamodb: Dynamodb;
+
+  public readonly ecs: ECS;
 
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    this.dynamodb = new Dynamodb(this, 'Dynamodb')
+    this.dynamodb = new Dynamodb(this, "Dynamodb");
 
+    this.ecs = new ECS(this, "ECS", {
+      dynamodb: this.dynamodb,
+    });
   }
 }
