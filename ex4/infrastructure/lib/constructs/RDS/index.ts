694aefd4dcbbdee15c1529c03b3fbf4cb6bc0102
pdTrade <pd.pub.dev@gmail.com>
2024-06-11 13:01:07 +0900

    RDS設定

---
+++ ex4/infrastructure/lib/constructs/RDS/index.ts
@@ -0,0 +1,29 @@
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as rds from 'aws-cdk-lib/aws-rds';
+import { Construct } from "constructs";
+
+interface Props {
+  vpc: ec2.Vpc;
+}
+
+export class RDS extends Construct {
+  public readonly instance: rds.DatabaseInstance;
+
+  constructor(scope: Construct, id: string, props: Props) {
+    super(scope, id)
+
+
+    this.instance = new rds.DatabaseInstance(scope, "MySQL-RDS", {
+      engine: rds.DatabaseInstanceEngine.mysql({
+        version: rds.MysqlEngineVersion.VER_8_0_36
+      }),
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.T2,
+        ec2.InstanceSize.SMALL,
+      ),
+      port: 3306,
+      vpc: props.vpc
+    })
+
+  }
+}
