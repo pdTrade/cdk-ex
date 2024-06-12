33cb88548deffd9e752b86b4a69bf5c5f1eb2451
pdTrade <pd.pub.dev@gmail.com>
2024-06-13 00:20:04 +0900

    S3バケット作成まで

--- ex4/infrastructure/lib/infrastructure-stack.ts
+++ ex4/infrastructure/lib/infrastructure-stack.ts
@@ -1,16 +1,31 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { Route53 } from './constructs/Route53';
+import { ACM } from './constructs/ACM';
+import { S3 } from './constructs/S3';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class InfrastructureStack extends cdk.Stack {
+  public readonly route53: Route53;
+  public readonly acm: ACM;
+  public readonly s3: S3;
+
+
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    this.route53 = new Route53(this, 'Route53');
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'InfrastructureQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    this.acm = new ACM(this, 'ACM', {
+      hosted_zone: this.route53.hosted_zone,
+    });
+
+    this.s3 = new S3(this, 'S3', {
+      acm: this.acm,
+      route53: this.route53,
+    });
+
   }
+
+  
 }
